87ed42043c283eb359209edaa05b316be84f7e3a
mms1410 <SvenMauriceMorlock@yahoo.com>
2021-11-14 16:56:43 +0100

    Protokoll Tim addon

--- R/01_data_preprocessing.R
+++ R/01_data_preprocessing.R
@@ -1,21 +1,19 @@
 ## Preliminaries
 library(readxl)
 library(checkmate)
-library(utils)
 ## Read data and create data variables
-folderpath <- choose.dir()
-filenames <- list.files(folderpath) # relative path
+path_data <- dirname(rstudioapi::getSourceEditorContext()$path)
+path_data <- dirname(path_data) # folder down from 'R' folder
+path_data <- paste0(path_data, .Platform$file.sep, "data") 
+filenames <- list.files(path_data) # relative path
 for (filename in filenames){
-  assertCharacter(filename) # ToDo: check pattern ?
   # create data reference
   # assumes data name of form '<alnum>_Daten<alnum>'
   # variable with name of <alnum> string preceding '_Daten' string will be created
-  pattern <- pattern <- "(?:(?!_Daten).)*" # neg look ahead nedded
+  pattern <- pattern <- "(?:(?!_Daten).)*" # neg look ahead needed
   varname <- regmatches(filename, gregexpr(pattern = pattern,
                                                 filename, perl = TRUE))[[1]][1]
-  assign(varname, read_excel(paste0(folderpath, filename))) # absolute path
+  assign(varname, read_excel(paste0(path_data,.Platform$file.sep, filename))) # absolute path
   # ToDo:
   # clean data
 }
-
-
